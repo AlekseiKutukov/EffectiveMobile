32424311917f908b8cf1a3e27163bcd0814e56f3
Aleksei_Kutukov <kytykob@gmail.com>
2025-12-06 20:31:16 +0300

    disain + adaptiv

--- frontend/src/components/MovieCard/MovieCard.jsx
+++ frontend/src/components/MovieCard/MovieCard.jsx
@@ -3,7 +3,9 @@ import styles from "./MovieCard.module.css";
 const MovieCard = ({ title, poster, year, description, rating, genre }) => {
   return (
     <div className={styles.card}>
-      <img src={poster} alt={title} className={styles.posterImage} />
+      <div className={styles.card__image}>
+        <img src={poster} alt={title} className={styles.posterImage} />
+      </div>
       <div className={styles.card__title}>{title}</div>
       <div className={styles.card__year}>{year}</div>
       {/* <div className={styles.card__genre}>{genre.join(", ")}</div>
